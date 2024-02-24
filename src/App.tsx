d51ecc29820cdaff04476944b1d4d8c263275777
Ta2shi <20121048@kaishi-pu.ac.jp>
2024-02-24 22:28:26 +0900

    muiライブラリの追加、各ディレクトリ構成の変更

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { PieGraph } from './components/PieChart';
+import { PieGraph } from './components/atoms/PieChart';
 
 
 function App() {
   return (
-    <PieGraph />
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.tsx</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    //   <div>
-    //     <h1>
-    //       Chart.js Pie Chart
-    //     </h1>
-    //     <PieGraph />
-    //   </div>
-    // </div>
+    <>hello, world</>
   );
 }
 
